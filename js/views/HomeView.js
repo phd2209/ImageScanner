1794a70da63992613519dfefe250d224da66673c
Toni Awwad <ithorn@gmail.com>
2014-11-12 20:08:26 +0100

    Revert "Revert "first upload""
    This reverts commit c5f07a6527cf413e7143065c18b853ecba2f90d9.

---
+++ js/views/HomeView.js
@@ -0,0 +1,9 @@
+﻿app.views.HomeView = Backbone.View.extend({
+    initialize: function () {
+        this.template = app.templateLoader.get('homeView');
+    },
+    render: function () {
+        this.$el.html(this.template());
+        return this;
+    }
+});
