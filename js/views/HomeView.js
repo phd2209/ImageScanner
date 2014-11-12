89961cf2d32f5b6c7c541de49c53d420e2ee2ef7
Toni Awwad <ithorn@gmail.com>
2014-11-12 20:06:29 +0100

    Revert "first upload"
    This reverts commit 00eecb55bc008d1dee9d6260078d0f43e3987fb5.

--- js/views/HomeView.js
+++
@@ -1,9 +0,0 @@
-﻿app.views.HomeView = Backbone.View.extend({
-    initialize: function () {
-        this.template = app.templateLoader.get('homeView');
-    },
-    render: function () {
-        this.$el.html(this.template());
-        return this;
-    }
-});
--- js/views/ResultView.js
+++
@@ -1,9 +0,0 @@
-﻿app.views.ResultView = Backbone.View.extend({
-    initialize: function () {
-        this.template = app.templateLoader.get('resultView');
-    },
-    render: function () {
-        this.$el.html(this.template());
-        return this;
-    }
-});
